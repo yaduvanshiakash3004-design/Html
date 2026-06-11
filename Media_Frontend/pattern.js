b7a64d5ad29c15d5c86a891ddf8ecb0fbd44db1c
yaduvanshiakash3004-design <yaduvanshiakash3004@gmail.com>
2026-06-11 15:01:11 +0530

    Internship

---
+++ Media_Frontend/pattern.js
@@ -0,0 +1,54 @@
+let N = 5;
+let totalRows = 2 * N - 1;
+
+for (let row = 1; row <= totalRows; row++) {
+    let v = 0;
+    if (row <= N) {
+        v = row;
+    } else {
+        v = totalRows - row + 1;
+    }
+    
+    let rowStr = "";
+
+    let leftLength = v;
+    for (let j = 0; j < leftLength; j++) {
+        if (row <= N) {
+            if (j % 2 === 0) {
+                rowStr += v + " ";
+            } else {
+                rowStr += (10 - v) + " ";
+            }
+        } else {
+            if (j % 2 === 0) {
+                rowStr += (10 - v) + " ";
+            } else {
+                rowStr += v + " ";
+            }
+        }
+    }
+
+    let spaces = (N - v) * 2; 
+    for (let s = 0; s < spaces; s++) {
+        rowStr += "  "; 
+    }
+
+    let rightLength = v; 
+    for (let j = 0; j < rightLength; j++) {
+        if (row <= N) {
+            if (j % 2 === 0) {
+                rowStr += (10 - v) + " ";
+            } else {
+                rowStr += v + " ";
+            }
+        } else {
+            if (j % 2 === 0) {
+                rowStr += v + " ";
+            } else {
+                rowStr += (10 - v) + " ";
+            }
+        }
+    }
+
+    console.log(rowStr);
+}
